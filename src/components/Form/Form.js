1aea620ef1de907b31f131fef1cb03f8ff9294f4
Ramanjs <raman.s2662@gmail.com>
2022-09-29 23:27:15 +0530

    updated premium page

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,6 @@ import {useState} from 'react';
 import './Form.css';
 
 const Form = () => {
-
   return (
     <div className="container form">
       <h1>World record event with 1000 thars</h1>
--- src/components/Premium/Premium.js
+++ src/components/Premium/Premium.js
@@ -1,10 +1,15 @@
+import './Premium.css';
+import premiumbanner from '../../assets/GoT Premium Banner.png';
+import premiumbannerMobile from '../../assets/got-premium-banner-mobile.png';
 import useMobile from '../../hooks/UseMobile';
 
 const Premium = () => {
   const isMobile = useMobile();
+
   return (
-    <div className="container">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScHMgSygb9gBvoaQj7LKhnEXY8c0wBzE5-Tp5Xt__eOFyJQwA/viewform?embedded=true" width={isMobile ? "320" : "640"} height="2776" frameborder="0" marginheight="0" marginwidth="0" className="premium">Loading…</iframe>
+    <div className="container premium">
+      <img src={isMobile ? premiumbannerMobile : premiumbanner} alt="premium banner" />
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLScHMgSygb9gBvoaQj7LKhnEXY8c0wBzE5-Tp5Xt__eOFyJQwA/viewform?usp=sf_link">Register for Premium</a>
     </div>
   )
 }
