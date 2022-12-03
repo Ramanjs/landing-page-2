12b034859d8a711af4041d26674869b72df556df
Ramanjs <raman.s2662@gmail.com>
2022-12-04 01:40:10 +0530

    add: integrate sanity cms for gallery

---
+++ content/schemas/gallery.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'gallery',
+  type: 'document',
+  title: 'Gallery',
+  fields: [
+    {
+      type: 'image',
+      name: 'image',
+      title: 'Image'
+    },
+    {
+      type: 'string',
+      name: 'image_name',
+      title: 'Image name'
+    },
+  ]
+}
