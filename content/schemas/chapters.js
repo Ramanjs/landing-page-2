1e80576d44bf531364fd2976da767016de55a640
Ramanjs <raman.s2662@gmail.com>
2022-12-05 00:21:20 +0530

    add: schema and frontend fetching for chapters

---
+++ content/schemas/chapters.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'chapters',
+  type: 'document',
+  title: 'Chapters',
+  fields: [
+    {
+      type: 'string',
+      name: 'chapter_name',
+      title: 'Chapter name'
+    }
+  ]
+}
--- src/components/Home/Chapters.js
+++ src/components/Home/Chapters.js
@@ -1,19 +1,33 @@
+import {useEffect} from 'react';
+import {useState} from 'react';
 import './Chapters.css';
 
 const Chapters = () => {
+  const [chapters, setChapters] = useState([])
+
+  const PROJECT_ID = "jqjakyhg";
+  const DATASET = "production";
+
+  useEffect(() => {
+    let QUERY = encodeURIComponent('*[_type == "chapters"] { chapter_name }')
+    fetch(`https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`)
+      .then(res => res.json())
+      .then(({ result }) => {
+        setChapters(result.map(chapter => chapter.chapter_name))
+      })
+  }, [])
+
   return (
     <div className="container chapters">
       <h2>GOT Chapters</h2>
       <p><b>Gang of Thars</b> has multiple chapters spread accross multiple cities in India which include:</p>
       <ul className="chapter-list">
-        <li className="chapter-point border-rt border-bt"><p>GoT</p> <p>Delhi NCR</p></li>
-        <li className="chapter-point border-rt border-bt"><p>GoT</p> <p>CHD</p></li>
-        <li className="chapter-point border-rt border-bt"><p>GoT</p> <p>BLR</p></li>
-        <li className="chapter-point border-bt"><p>GoT</p> <p>Mumbai</p></li>
-        <li className="chapter-point border-rt border-bt"><p>GoT</p> <p>Himachal</p></li>
-        <li className="chapter-point border-rt border-bt"><p>GoT</p> <p>Haryana</p></li>
-        <li className="chapter-point border-rt border-bt"><p>GoT</p> <p>Gujrat</p></li>
-        <li className="chapter-point border-bt"><p>GoT</p> <p>UP</p></li>
+        {chapters.map((chapter, index) => {
+          const classname = (index % 4 == 3) ? "" : " border-rt"; 
+          return (
+            <li className={"chapter-point border-bt" + classname}><p>GoT</p> <p>{chapter}</p></li>
+          )
+        })}
       </ul>
       <p>CALL/WHATSAPP TO JOIN:</p>
       <p style={{marginTop: '0'}}><b>+919990020099</b></p>
