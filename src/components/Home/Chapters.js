683820bf4f9c339e8e5fae3e15bbbe2b3b45ca5c
Ramanjs <raman.s2662@gmail.com>
2023-02-13 03:12:38 +0530

    add: load more feature to chapters

--- src/components/Home/Chapters.js
+++ src/components/Home/Chapters.js
@@ -4,6 +4,7 @@ import './Chapters.css';
 
 const Chapters = () => {
   const [chapters, setChapters] = useState([])
+  const [expand, setExpand] = useState(false)
 
   const PROJECT_ID = "jqjakyhg";
   const DATASET = "production";
@@ -22,13 +23,14 @@ const Chapters = () => {
       <h2>GOT Chapters</h2>
       <p><b>Gang of Thars</b> has multiple chapters spread accross multiple cities in India which include:</p>
       <ul className="chapter-list">
-        {chapters.map((chapter, index) => {
+        {chapters && chapters.slice(0, (expand ? chapters.length : 4)).map((chapter, index) => {
           const classname = (index % 4 == 3) ? "" : " border-rt"; 
           return (
             <li className={"chapter-point border-bt" + classname}><p>GoT</p> <p>{chapter}</p></li>
           )
         })}
       </ul>
+      {!expand && <div className="load" onClick={() => setExpand(true)}>Load More</div>}
       <p>CALL/WHATSAPP TO JOIN:</p>
       <p style={{marginTop: '0'}}><b>+919990020099</b></p>
     </div>
