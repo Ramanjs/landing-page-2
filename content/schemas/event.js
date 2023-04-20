d0b3e40c5aa8e101ab1e038622682a32fd6cf385
Ramanjs <raman.s2662@gmail.com>
2023-04-21 01:12:50 +0530

    add event schema to sanity

---
+++ content/schemas/event.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image'
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'button_name',
+      title: 'Button name',
+      type: 'string'
+    },
+    {
+      name: 'button_url',
+      title: 'Button url',
+      type: 'string'
+    }
+  ]
+}
--- content/schemas/schema.js
+++ content/schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import gallery from './gallery'
 import partners from './partners'
 import chapters from './chapters'
+import event from './event'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,6 +17,7 @@ export default createSchema({
   types: schemaTypes.concat([
     gallery,
     partners,
-    chapters
+    chapters,
+    event
   ]),
 })
